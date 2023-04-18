3af71b95d6f91142354775386428a8cde38a4049
Hectorzilla <hectortra123@gmail.com>
2023-04-17 23:02:35 -0600

    inventario funcional pero con slots, usa el pick up object

---
+++ Assets/Scripts/Hector/inventory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Hector/itemPickup.cs
+++ Assets/Scripts/Hector/itemPickup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class itemPickup : MonoBehaviour
@@ -9,13 +10,18 @@ public class itemPickup : MonoBehaviour
 
     [SerializeField]
     private GameObject pickUpText;
-
+    private inventory Inventory;
+    private GameObject pruebasVar;
+    public GameObject itemButton;
     private bool pickUpAllowed;
+    int numero = 0;
 
     // Use this for initialization
     private void Start()
     {
         pickUpText.gameObject.SetActive(false);
+
+        Inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<inventory>();
     }
 
     // Update is called once per frame
@@ -45,7 +51,25 @@ public class itemPickup : MonoBehaviour
 
     private void PickUp()
     {
-        Destroy(gameObject);
+
+        for (int i = 0; i < Inventory.slots.Length; i++)
+        {
+            if (Inventory.isFull[i] == false)
+            {
+                //se puede recoger y asi xd
+                Instantiate(itemButton, Inventory.slots[i].transform, false);
+                Inventory.isFull[i] = true;
+                Destroy(gameObject);
+                break;
+            }
+        }
+       
+    }
+
+   public void pruebaScore()
+    {
+        numero = numero + 1;
+        pruebasVar.GetComponent<UnityEngine.UI.Text>().text = "Score : " + numero;
     }
 
 }
